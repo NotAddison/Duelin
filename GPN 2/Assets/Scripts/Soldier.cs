ff1cda190356249e5cd25e479f01846912ddff53
realelongatedmusket <s10222819@connect.np.edu.sg>
2022-08-07 10:18:29 +0800

    Add: Hivemind unit perk

--- GPN 2/Assets/Scripts/Soldier.cs
+++ GPN 2/Assets/Scripts/Soldier.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Soldier : BaseGoblin
 {
-    public override void UseAbility()
-    {
-    }
-    public override void UsePassive()
-    {
-        
+    void Start() {
+        LocalInventory.getInstance().GetGoblins().ForEach(x => {
+            if (x.GetType() == typeof(Soldier)) {
+                LocalInventory.getInstance().AddGold(1);
+            }
+            else if (x.GetType() == typeof(Captain)) {
+                LocalInventory.getInstance().AddGold(1);
+            }
+            else if (x.GetType() == typeof(Warrior)) {
+                LocalInventory.getInstance().AddGold(1);
+            }
+        });
     }
 }
