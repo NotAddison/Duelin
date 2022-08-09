dda062c88cc1e4bd69cb00379e02391353d20305
realelongatedmusket <s10222819@connect.np.edu.sg>
2022-08-09 23:07:53 +0800

    Fix: Javlin ability and hive perk deducting gold

--- GPN 2/Assets/Scripts/Entities/Goblins/Shielder.cs
+++ GPN 2/Assets/Scripts/Entities/Goblins/Shielder.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Photon.Pun;
 
 public class Shielder : Tank
 {
+    private BaseGoblin entity;
     public override int Cost() => 3;
+    public override void UsePassive()
+    {
+        base.UsePassive();
+    }
     public override void UseAbility(InputAction.CallbackContext context)
     {
         
     }
-    public override void UsePassive()
-    {
-        base.UsePassive();
-        // [TODO] Implement knockback of attacked entity
-    }
+
 }
